1ad5302bb75db24af9a45dea5b2fc8455c4e4b9f
flpa <flpa.dev@zoho.com>
2015-02-08 18:26:13 +0100

    floodfill: Works, but ugly

--- 200-floodfill/floodfill.lisp
+++ 200-floodfill/floodfill.lisp
@@ -1,6 +1,27 @@
 (defpackage #:200-floodfill
-  (:use #:cl) 
+  (:use #:cl))
 
 (in-package #:200-floodfill)
 
-
+(labels ((neighbours (x y width height)
+           (remove-if-not #'(lambda (candidate)
+                              (and (<= 0 (car candidate) (1- width)) 
+                                   (<= 0 (cdr candidate) (1- height))))
+                          (list (cons (1- x) y)
+                                (cons (1+ x) y)
+                                (cons x (1- y))
+                                (cons x (1+ y)))))
+         (floodfill (frontier symbol lines width height)
+           (if frontier
+             (let ((x (caar frontier))
+                   (y (cdar frontier)))
+               (if (char-equal #\. (elt (elt lines y) x))
+                 (progn
+                   (setf (elt (elt lines y) x) symbol)
+                   (floodfill (append (rest frontier) (neighbours x y width height)) symbol lines width height))
+                 (floodfill (rest frontier) symbol lines width height)))
+             (format t "~{~a~%~}" lines))))
+  (with-open-file (s  "~/code/misc/reddit-daily/200-floodfill/res/01.in" :direction :input)
+    (loop with width = (read s) and height = (read s)
+      for i from 1 to height collecting (read-line s) into lines
+      finally (floodfill (list (cons (read s) (read s))) (read-char s) lines width height))))
