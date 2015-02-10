44cf9be32cb85278044e348c8efcc9befcd92554
flpa <flpa.dev@zoho.com>
2015-02-10 13:07:17 +0100

    196: work on wrapping operands

--- 196-precedence-parsing/precedence-parsing.lisp
+++ 196-precedence-parsing/precedence-parsing.lisp
@@ -1,31 +1,67 @@
 (in-package #:precedence-parsing)
 
-(defun read-it (stream symbols)
-  "Read from stream recursively resolving lists."
-  (let ((in (read-char stream nil #\))))
+;; assume: only perfect input
+;; assume: only single-digit numbers (...)
+
+(defun read-it (symbols)
+  "Read from standard in recursively resolving lists."
+  (let ((in (read-char *standard-input* nil #\))))
     (if (or (eql in #\)) (eql in #\Newline))
       (reverse symbols)
-      (read-it stream (cons 
-                        (if (eql in #\()
-                          (read-it stream nil)
-                          in) 
-                        symbols)))))
+      (read-it (cons 
+                 (if (eql in #\()
+                   (read-it nil)
+                   in) 
+                 symbols)))))
 
-(let ((operators nil))
-  (with-open-file (s "~/code/misc/reddit-daily/196-precedence-parsing/res/1.in")
-    (dotimes (i (parse-integer (read-line s)))
-      (let ((line (read-line s)))
-        (setf operators (acons (elt line 0) (eql #\r (elt line 2)) operators)))
-      )
-    ;; now we have an alist symbol->right-associative-p
-    ;; next: read input and start checking
-    ))
+(with-open-file (*standard-input* "~/code/misc/reddit-daily/196-precedence-parsing/res/1.in")
+  (make-clear))
+
+(defun make-clear ()
+  (let ((operators nil))
+    (flet ((parse-operator (line)
+             (acons (elt line 0) (eql #\r (elt line 2)) operators)))
+      (dotimes (i (parse-integer (read-line)))
+        (setf operators (parse-operator (read-line))))
+      ;; now we have an alist symbol->right-associative-p
+      ;; next: read input and start checking
+      (pos-of-strongest-operator (reverse operators) (read-it nil)))))
+
+;;e.g. (wrap-at '(1 + 2 + 3) 3)  => (1 + (2 + 3))
+(defun wrap-at (term op-pos)
+  (append (subseq term 0 (1- op-pos))
+          (list (subseq term (1- op-pos) (+ op-pos 2)))
+          (subseq term (+ op-pos 2))))
+
+(defun pos-of-strongest-operator (operators term)
+  (operator-pos (car (remove-if #'(lambda (op)
+                                    (not (find op term))) 
+                                operators 
+                                :key #'car )) 
+                term))
+
+(defun operator-pos (op term)
+  (position (car op) term :from-end (cdr op)))
+
+(defun fix-it (operators term)
+  (if (listp term)
+    (if (> (length term) 3)
+      ;; > 3: there gotta be an operator
+      (fix-it operators (wrap-at term ))
 
 
-; sublists > 3
+
+      ;;fixmagic
+      (mapcar #'(lambda (symbol)
+                  (if (listp symbol)
+                    (fix-it operators symbol)
+                    (symbol)))))
+    (list term))
+  ;; if length > 3: need fix
+  ;;  fix = append sublist before, list wrapping sublist, sublist after
+  )
+
+
 ; find strongest
 ; find associative
 ; wrap in new list
-
-
-
