e58ddc0cc566f3e9d188ea874e381a20126139cb
flpa <flpa.dev@zoho.com>
2015-02-10 13:58:31 +0100

    196: Improve naming

--- 196-precedence-parsing/precedence-parsing.lisp
+++ 196-precedence-parsing/precedence-parsing.lisp
@@ -3,30 +3,29 @@
 ;; assume: only perfect input
 ;; assume: only single-digit numbers (...)
 
-(defun read-it (symbols)
-  "Read from standard in recursively resolving lists."
+(defun read-term (symbols)
+  "Read from standard-input, recursively resolving lists."
   (let ((in (read-char *standard-input* nil #\))))
     (if (or (eql in #\)) (eql in #\Newline))
       (reverse symbols)
-      (read-it (cons 
-                 (if (eql in #\()
-                   ;; read sub-list
-                   (read-it '())
-                   in) 
-                 symbols)))))
+      (read-term (cons 
+                   (if (eql in #\()
+                     ;; read sub-list
+                     (read-term '())
+                     in) 
+                   symbols)))))
 
 (with-open-file (*standard-input* "~/code/misc/reddit-daily/196-precedence-parsing/res/5.in")
-  (make-clear))
+  (main))
 
-(defun make-clear ()
+(defun main ()
   (let ((operators nil))
     (flet ((parse-operator (line)
              (acons (elt line 0) (eql #\r (elt line 2)) operators)))
       (dotimes (i (parse-integer (read-line)))
         (setf operators (parse-operator (read-line))))
       ;; now we have an alist symbol->right-associative-p
-      ;; next: read input and start checking
-      (format t "~a~%" (fix-it (reverse operators) (read-it '()))))))
+      (format t "~a~%" (disambiguate (reverse operators) (read-term '()))))))
 
 ;;e.g. (wrap-at '(1 + 2 + 3) 3)  => (1 + (2 + 3))
 (defun wrap-at (term op-pos)
@@ -44,14 +43,14 @@
 (defun operator-pos (op term)
   (position (car op) term :from-end (cdr op)))
 
-(defun fix-it (operators term)
+(defun disambiguate (operators term)
   (if (listp term)
     (if (> (length term) 3) ; assume there's an operator
-      (fix-it operators (wrap-at term (pos-of-strongest-operator operators term)))
+      (disambiguate operators (wrap-at term (pos-of-strongest-operator operators term)))
       ; fix nested lists
       (mapcar #'(lambda (symbol)
                   (if (listp symbol)
-                    (fix-it operators symbol)
+                    (disambiguate operators symbol)
                     symbol))
               term))
     (list term)))
