37112bbccbc489b774adc2fb9fc04f25f3bc2c66
flpa <flpa.dev@zoho.com>
2015-02-10 13:50:22 +0100

    196: first working version for test1, add custom test

--- 196-precedence-parsing/precedence-parsing.lisp
+++ 196-precedence-parsing/precedence-parsing.lisp
@@ -10,11 +10,12 @@
       (reverse symbols)
       (read-it (cons 
                  (if (eql in #\()
-                   (read-it nil)
+                   ;; read sub-list
+                   (read-it '())
                    in) 
                  symbols)))))
 
-(with-open-file (*standard-input* "~/code/misc/reddit-daily/196-precedence-parsing/res/1.in")
+(with-open-file (*standard-input* "~/code/misc/reddit-daily/196-precedence-parsing/res/5.in")
   (make-clear))
 
 (defun make-clear ()
@@ -25,7 +26,7 @@
         (setf operators (parse-operator (read-line))))
       ;; now we have an alist symbol->right-associative-p
       ;; next: read input and start checking
-      (pos-of-strongest-operator (reverse operators) (read-it nil)))))
+      (format t "~a~%" (fix-it (reverse operators) (read-it '()))))))
 
 ;;e.g. (wrap-at '(1 + 2 + 3) 3)  => (1 + (2 + 3))
 (defun wrap-at (term op-pos)
@@ -45,23 +46,12 @@
 
 (defun fix-it (operators term)
   (if (listp term)
-    (if (> (length term) 3)
-      ;; > 3: there gotta be an operator
-      (fix-it operators (wrap-at term ))
-
-
-
-      ;;fixmagic
+    (if (> (length term) 3) ; assume there's an operator
+      (fix-it operators (wrap-at term (pos-of-strongest-operator operators term)))
+      ; fix nested lists
       (mapcar #'(lambda (symbol)
                   (if (listp symbol)
                     (fix-it operators symbol)
-                    (symbol)))))
-    (list term))
-  ;; if length > 3: need fix
-  ;;  fix = append sublist before, list wrapping sublist, sublist after
-  )
-
-
-; find strongest
-; find associative
-; wrap in new list
+                    symbol))
+              term))
+    (list term)))
