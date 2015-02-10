bd574873793d95a43b1e64cb80c32eca13ef60f0
flpa <flpa.dev@zoho.com>
2015-02-10 10:27:16 +0100

    196: alist building complete

--- 196-precedence-parsing/precedence-parsing.lisp
+++ 196-precedence-parsing/precedence-parsing.lisp
@@ -1,22 +1,25 @@
 (in-package #:precedence-parsing)
 
 (defun read-it (stream symbols)
+  "Read from stream recursively resolving lists."
   (let ((in (read-char stream nil #\))))
-    (if (char-equal in #\))
+    (if (or (eql in #\)) (eql in #\Newline))
       (reverse symbols)
       (read-it stream (cons 
-                        (if (char-equal in #\()
+                        (if (eql in #\()
                           (read-it stream nil)
                           in) 
                         symbols)))))
 
-(with-open-file (s "~/code/misc/reddit-daily/196-precedence-parsing/res/1.in")
-           (read-line s)
-           (aref (read-line s) 0) 
-           (aref (read-line s) 0) 
-           (aref (read-line s) 0)
-           (read-it s nil)
-           )
+(let ((operators nil))
+  (with-open-file (s "~/code/misc/reddit-daily/196-precedence-parsing/res/1.in")
+    (dotimes (i (parse-integer (read-line s)))
+      (let ((line (read-line s)))
+        (setf operators (acons (elt line 0) (eql #\r (elt line 2)) operators)))
+      )
+    ;; now we have an alist symbol->right-associative-p
+    ;; next: read input and start checking
+    ))
 
 
 ; sublists > 3
